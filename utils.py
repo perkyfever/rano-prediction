464937594fc954e3fce4976c51ea0543108afaf5
Daniil Tikhonov <dteakhperky@gmail.com>
2025-07-24 21:13:44 +0000

    update: new utils

--- utils.py
+++ utils.py
@@ -3,18 +3,25 @@ import random
 import numpy as np
 import torch
 
+RANDOM_SEED = 0xBAD5EED
 
-def seed_everything(seed=0xBAD5EED):
-    random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
+CLASS_TO_RESPONSE = {0: "CR", 1: "PR", 2: "SD", 3: "PD"}
+RESPONSE_TO_CLASS = {"CR": 0, "PR": 1, "SD": 2, "PD": 3}
+
+
+def seed_everything() -> torch.Generator:
+    random.seed(RANDOM_SEED)
+    os.environ["PYTHONHASHSEED"] = str(RANDOM_SEED)
+    np.random.seed(RANDOM_SEED)
+    torch.manual_seed(RANDOM_SEED)
+    torch.cuda.manual_seed(RANDOM_SEED)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
 
     generator = torch.Generator()
-    generator.manual_seed(seed)
+    generator.manual_seed(RANDOM_SEED)
 
     if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
+        torch.cuda.manual_seed_all(RANDOM_SEED)
+
+    return generator
